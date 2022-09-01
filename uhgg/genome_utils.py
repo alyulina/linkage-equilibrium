4602d8ecd9d7de2c2ca795644f4681945bba9154
alyulina <a.s.lyulina@gmail.com>
2022-09-01 16:31:21 -0700

    Rearraging the folder

---
+++ uhgg/genome_utils.py
@@ -0,0 +1,15 @@
+import os
+import math
+import pandas as pd
+import numpy as np
+
+def reverse_complement():
+    return
+
+def get_genome_sequence():
+    return
+
+def get_genome_annotation():
+    return
+
+def
