60cf01353220c807ec2ce798121221dc798b6727
alyulina <a.s.lyulina@gmail.com>
2022-09-23 18:09:42 -0700

    Changed .sh script

--- uhgg/run_annotate_snps.sh
+++ uhgg/run_annotate_snps.sh
@@ -8,6 +8,7 @@
 #SBATCH --mail-user=alyulina@stanford.edu
 #SBATCH --mail-type=NONE
 #SBATCH --no-requeue
+#SBATCH --array=1-10
 #SBATCH --export=NONE
 unset SLURM_EXPORT_ENV
 export OMP_NUM_THREADS=1
@@ -17,4 +18,5 @@ module load py-numpy/1.18.1_py36
 module load viz
 module load py-pandas/1.0.3_py36
 #
-srun --cpu_bind=verbose python3 annotate_snps.py
+echo $SLURM_ARRAY_TASK_ID
+srun --cpu_bind=verbose python3 annotate_snps.py -s $(head -$SLURM_ARRAY_TASK_ID species_accessions.txt | tail -1)
